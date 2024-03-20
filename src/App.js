709df3ea0b5e86f4281780fc46e16110f2f9a070
zonairshah <126079813+zonairshah@users.noreply.github.com>
2024-03-20 15:40:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> This is the testing for github actions with S3 </h1>        
+        <h1> This is the testing for github actions with S3 and aws </h1>        
         <a
           className="App-link"
           href="https://reactjs.org"
